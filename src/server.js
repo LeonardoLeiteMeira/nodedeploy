531b5b4694e37e1ca3053204d90f6e1f3219f1f9
Leonardo Leite Meira dos Santos <leonardo.meira@dtidigital.com.br>
2020-06-07 23:10:48 -0300

    some changes

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
-const express = require('express');
-const routes = require('./routes');
+require("dotenv/config"); //le as variaveis de ambiente no .env e deixa disponivel na aplicação
 
-require('./database');
+const express = require("express");
+const routes = require("./routes");
+
+require("./database");
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(process.env.PORT || 3333);
